7bdc02228a3c89b00170d2e46dc4a8293c189cf0
victoracho <victoracho.box@gmail.com>
2024-02-15 17:08:54 +0000

    Inicializando el proyecto

---
+++ app/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center"> </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import axios from "axios";
+
+export default defineComponent({
+  name: "IndexPage",
+  setup() {
+    axios.get("http://127.0.0.1:8000/sanctum/csrf-cookie").then((response) => {
+      console.log(response);
+    });
+  },
+});
+</script>
