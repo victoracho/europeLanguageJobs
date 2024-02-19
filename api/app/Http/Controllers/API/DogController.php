a5b5f7077c6c1eb69766b94d42064d3f4ab553a0
victoracho <victoracho.box@gmail.com>
2024-02-19 09:32:08 +0000

    arreglando bugs

--- api/app/Http/Controllers/API/DogController.php
+++ api/app/Http/Controllers/API/DogController.php
@@ -68,6 +68,13 @@ class DogController extends BaseController
             );
             $result =  $query->get();
             $breeds = $result;
+            foreach ($breeds as $clas) {
+                $object = (object)[];
+                $object->value = $clas->name;
+                $object->label = $clas->name;
+                $object->id = $clas->id;
+                $clas->classification = $object;
+            }
             foreach ($breeds as $bree) {
                 $bree->photo = env('APP_URL') . '/storage/dogs/' . $bree->photo;
             }
