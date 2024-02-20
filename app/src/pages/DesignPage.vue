3c855f0d9e5364106320c51520ce98180bfd48b1
victoracho <victoracho.box@gmail.com>
2024-02-20 00:49:30 +0000

    subiendo desing.vue

--- app/src/pages/DesignPage.vue
+++ app/src/pages/DesignPage.vue
@@ -166,7 +166,8 @@
           <div class="page-container layout-padding bg-blue">
             <div class="row-banner-container">
               <div class="row justify-center full-width items-center">
-                <h6>Together, your hires, our goal!</h6>
+                <h6 style="color: white">Together, your hires, our goal!</h6>
+
                 &nbsp; &nbsp; &nbsp;
                 <q-btn
                   unelevated
